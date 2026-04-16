599195d8d7912da5f25c2905101fb322523ad63f
Davide Domini <davide.domini@unibo.it>
2026-04-16 19:30:10 +0200

    feat: add empty methods to be implemented in dt

--- src/distributed/DT.py
+++ src/distributed/DT.py
@@ -1,13 +1,14 @@
 import pandas as pd
 from torch import nn
 from datetime import datetime
-
+from src.distributed.DTAggregate import DTAggregate
 
 class DT:
 
     def __init__(self, mid: str, data_path: str):
         self.mid = mid
         self._model = None
+        self._dt_aggregate = None
         self.data = pd.read_csv(f'{data_path}/{mid}.csv')
         self.activation_date, self.stopping_date = self.__get_activation_interval()
 
@@ -24,3 +25,18 @@ class DT:
     @model.setter
     def model(self, model: nn.Module):
         self._model = model
+
+    @dt_aggregate.setter
+    def dt_aggregate(self, dt: DTAggregate):
+        self._dt_aggregate = dt
+
+    ## TODO - implement this shit
+
+    def inference(self):
+        pass
+
+    def test_loader_from_data(self):
+        pass
+
+    def export_test_metrics(self):
+        pass
