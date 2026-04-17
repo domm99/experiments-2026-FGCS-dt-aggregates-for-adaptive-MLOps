3ed8fc9bacfda098d49ded1f3eaab3b60be8cb70
davidedomini <davide.domini@unibo.it>
2026-04-17 14:46:53 +0200

    feat: more learning configs

--- src/distributed/LearningConfig.py
+++ src/distributed/LearningConfig.py
@@ -1,13 +1,27 @@
 import torch
 from dataclasses import dataclass
 
+def get_current_device():
+    device: str = 'cpu'
+    if torch.accelerator.is_available():
+        current_accelerator = torch.accelerator.current_accelerator()
+        if current_accelerator is not None:
+            device = current_accelerator.type
+    return torch.device(device)
+
+
 @dataclass
 class LearningConfig:
-
-    sequence_length: int = 12
-    prediction_horizon: int = 3
-    train_ratio: float = 0.8
+    layers: int = 1
+    epochs: int = 12
+    stride: int = 12
+    dropout: float = 0.0
+    hidden_size: int = 64
+    batch_size: int = 256
     val_ratio: float = 0.2
-    device: torch.device = torch.device("cpu") ## TODO fix this
-    stride: int = 1, ## TODO fix
-    batch_size: int = 1,  ## TODO fix
+    train_ratio: float = 0.8
+    sequence_length: int = 12
+    learning_rate: float = 1e-3
+    prediction_horizon: int = 3
+    data_export_path: str = 'data'
+    device: torch.device = get_current_device()
