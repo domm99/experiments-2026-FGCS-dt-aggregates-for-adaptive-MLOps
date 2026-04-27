a0d453169f9cabc31da7fe153077f5d4895372d3
davidedomini <davide.domini@unibo.it>
2026-04-27 16:02:19 +0200

    fix: removing deploy on dockerhub

--- release.config.js
+++ release.config.js
@@ -1,10 +1,9 @@
 var prepareCmd = `
 echo VERSION="\${nextRelease.version}" > .env
-echo PROJECT_NAME= 'Experiments on Neighbor-based Transfer Learning in MARL' >> .env
+echo PROJECT_NAME= 'Experiments on FGCS DT Aggregates for Adaptive MLOps' >> .env
 docker build -t davidedomini99/experiments-2025-tcd-marl-neighbor-tl:\${nextRelease.version} .
 `
 var publishCmd = `
-docker push davidedomini99/experiments-2025-tcd-marl-neighbor-tl:\${nextRelease.version}
 git add .env
 git commit -m "chore(release): update .env versions to \${nextRelease.version} [skip ci]"
 git push
